57dea6bb8de4337081b862496deeb2a1c21d525a
Yanki Reichman <avireichman13@gmail.com>
2024-01-03 01:18:02 +0200

    add pie and more dashboard

--- src/Pages/Components/PieChart/PieChart.js
+++ src/Pages/Components/PieChart/PieChart.js
@@ -1,47 +1,103 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
+
 import "./PieChart.css";
 
-const PieChart = () => {
-    const [values, setValues] = useState([30, 40, 30]); // Example values for each section
+function PieChart({ listData }) {
+    const [pieElements, setPieElements] = useState([]);
 
-    const total = values.reduce((acc, value) => acc + value, 0);
+    var color = [
+        "forestgreen",
+        "crimson",
+        "olivedrab",
+        "cornflowerblue",
+        "orange",
+        "tomato",
+        "purple",
+        "turquoise",
+        "navy",
+        "gray",
+    ];
 
-    const renderSlices = () => {
-        let cumulativePercentage = 0;
+    function sliceSize(dataNum, dataTotal) {
+        return (dataNum / dataTotal) * 360;
+    }
 
-        return values.map((value, index) => {
-            const percentage = (value / total) * 100;
+    function addSlice(sliceSize, offset, sliceID, color) {
+        offset = offset - 1;
+        const sizeRotation = -179 + sliceSize;
+        const newElement = (
+            <div
+                className={`slice ${sliceID}`}
+                key={`slice ${sliceID}`}
+                style={{
+                    transform: `rotate(${offset}deg) translate3d(0,0,0)`,
+                }}>
+                <span
+                    style={{
+                        transform: `rotate(${sizeRotation}deg) translate3d(0,0,0)`,
+                        backgroundColor: color,
+                    }}></span>
+            </div>
+        );
+        setPieElements((elements) => [...elements, newElement]);
+    }
 
-            const sliceStyle = {
-                transform: `rotate(${cumulativePercentage}deg) skewY(${percentage}deg)`,
-                backgroundColor: getRandomColor(),
-            };
+    function iterateSlices(sliceSize, offset, dataCount, sliceCount, color) {
+        var maxSize = 179,
+            sliceId = `s${dataCount}-${sliceCount}`;
 
-            cumulativePercentage += percentage;
-
-            return <div key={index} className="slice" style={sliceStyle}></div>;
-        });
-    };
-
-    const getRandomColor = () => {
-        const letters = "0123456789ABCDEF";
-        let color = "#";
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
+        if (sliceSize <= maxSize) {
+            addSlice(sliceSize, offset, sliceId, color);
+        } else {
+            addSlice(maxSize, offset, sliceId, color);
+            iterateSlices(
+                sliceSize - maxSize,
+                offset + maxSize,
+                dataCount,
+                sliceCount + 1,
+                color
+            );
         }
-        return color;
-    };
+    }
+
+    function createPie() {
+        let listTotal = 0;
+        for (var i = 0; i < listData.length; i++) {
+            listTotal += listData[i][1];
+        }
+        let offset = 0;
+        for (i = 0; i < listData.length; i++) {
+            let size = sliceSize(listData[i][1], listTotal);
+            iterateSlices(size, offset, i, 0, color[i]);
+            offset += size;
+        }
+    }
+
+    useEffect(() => {
+        setPieElements([]);
+        createPie(); // eslint-disable-next-line
+    }, [listData]);
 
     return (
-        <div className="pie-chart">
-            <div className="chart">
-                {renderSlices()}
-                <div className="center">
-                    <div className="total">{total}</div>
-                </div>
+        <div className="PieChart">
+            <ul className="pieID legend" key={`pie-list`}>
+                {listData.map((arr, index) => {
+                    return (
+                        <li
+                            style={{ borderColor: color[index] }}
+                            key={`list-item-${arr[0]}-${index}`}>
+                            <div className="legend-name">{arr[0]}</div>
+                            <div className="legend-amount">{arr[1]}</div>
+                        </li>
+                    );
+                })}
+            </ul>
+            <div className="pieID pie" key={`pie-elements`}>
+                {pieElements}
             </div>
+            <div className="pie-shadow"></div>
         </div>
     );
-};
+}
 
 export default PieChart;
--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 //Context
 import { useAuth } from "../../hooks/auth/auth.js";
@@ -12,13 +13,27 @@ import "./Dashboard.css";
 import PieChart from "../Components/PieChart/PieChart.js";
 
 function Dashboard() {
+    const { axiosApi } = useAuth();
+    const cookies = useCookies()[0];
     const { pageName } = useParams();
     const [selectedOption, setSelectedOption] = useState(0);
     const pageNames = useContext(PageContext).pageNames;
     const [pieData, setPieData] = useState(
-        Object.fromEntries(Object.keys(pageNames).map((x) => [x, [""]]))
+        Object.fromEntries([
+            ...Object.keys(pageNames).map((x) => [x, [""]]),
+            [
+                "BuildingsData",
+                [{ amount: 1, amount_in_debt: 1, amount_paid: 1 }],
+            ],
+            [
+                "ApartmentsData",
+                [{ amount: 1, amount_in_debt: 1, amount_paid: 1 }],
+            ],
+            ["PeopleData", [{ amount: 1, amount_in_debt: 1, amount_paid: 1 }]],
+        ])
     );
-    const { axiosApi } = useAuth();
+
+    // console.log(pieData);
 
     const allOptions = ["Buildings", "Apartments", "People"];
 
@@ -26,7 +41,7 @@ function Dashboard() {
         const fetchAllData = async () => {
             try {
                 const res = await axiosApi.get("/info/pieInfo");
-                console.log(res);
+                // console.log(res);
                 setPieData(res.data);
             } catch (error) {
                 console.log(error);
@@ -48,7 +63,6 @@ function Dashboard() {
                 return null;
             }
         });
-        console.log(children);
         if (!children[0]) {
             return (
                 <div className="Dashboard-no-results">
@@ -61,27 +75,66 @@ function Dashboard() {
 
     return (
         <div className="Dashboard">
-            <div className="Dashboard-tile box-shadow">
-                <div
-                    className="Dashboard-tile-title"
-                    onClick={() =>
-                        setSelectedOption(
-                            selectedOption + 1 > allOptions.length - 1
-                                ? 0
-                                : selectedOption + 1
-                        )
-                    }>
-                    {pageNames[allOptions[selectedOption]]}{" "}
+            <div className="Dashboard-inner-nav box-shadow">
+                שלום, {cookies.name}
+            </div>
+            <div className="Dashboard-statistics">
+                <div className="Dashboard-statistic tile box-shadow">
+                    סה"כ בנייינים: {pieData["BuildingsData"][0].amount}
                 </div>
-                <div className="Dashboard-tile-body">
-                    {dashboardLines(allOptions[selectedOption])}
+                <div className="Dashboard-statistic tile box-shadow">
+                    סה"כ דירות: {pieData["ApartmentsData"][0].amount}
+                </div>
+                <div className="Dashboard-statistic tile box-shadow">
+                    סה"כ אנשים: {pieData["PeopleData"][0].amount}
                 </div>
             </div>
-            <div className="Dashboard-tile box-shadow">
-                <PieChart />
+            <div className="Dashboard-charts">
+                <div className="Dashboard-chart tile box-shadow">
+                    <div
+                        className="Dashboard-tile-title"
+                        onClick={() =>
+                            setSelectedOption(
+                                selectedOption + 1 > allOptions.length - 1
+                                    ? 0
+                                    : selectedOption + 1
+                            )
+                        }>
+                        {pageNames[allOptions[selectedOption]]}{" "}
+                    </div>
+                    <div className="Dashboard-tile-body">
+                        {dashboardLines(allOptions[selectedOption])}
+                    </div>
+                </div>
+                <div className="Dashboard-chart tile box-shadow">
+                    <div
+                        className="Dashboard-tile-title"
+                        onClick={() =>
+                            setSelectedOption(
+                                selectedOption + 1 > allOptions.length - 1
+                                    ? 0
+                                    : selectedOption + 1
+                            )
+                        }>
+                        {pageNames[allOptions[selectedOption]]}{" "}
+                    </div>
+                    <PieChart
+                        listData={[
+                            [
+                                "שילמו",
+                                pieData[`${allOptions[selectedOption]}Data`][0]
+                                    .amount_paid,
+                            ],
+                            [
+                                "לא שילמו",
+                                pieData[`${allOptions[selectedOption]}Data`][0]
+                                    .amount_in_debt,
+                            ],
+                        ]}
+                    />
+                </div>
+                <div className="Dashboard-chart tile box-shadow"></div>
             </div>
-            <div className="Dashboard-tile box-shadow"></div>
-            <div className="Dashboard-tile box-shadow"></div>
         </div>
     );
 }
